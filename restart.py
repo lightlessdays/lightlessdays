a71a70cce937d71134627c1178101b24ed5d600b
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-12-18 18:48:10 +0530

    Reverted Changes
    This reverts commit 89812a85073393220fc583200229dce4c583e1e0.

--- restart.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-
-frame = pd.DataFrame({'a': range(7), 'b': range(7, 0, -1), 'c': ['one', 'one', 'one', 'two', 'two', 'two', 'two'],'d': [0, 1, 2, 0, 1, 2, 3]})
-print(frame.set_index(['c','d']).reset_index())
